9e3ec0a8c4977af47b18e6d57966ab6bf052d9c0
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-08-12 12:04:59 -0700

    Changes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 #Using Puppet, create a file in /tmp
-file { 'FileCreation':
-  ensure  => file, #accepts any form of file existence, and creates a normal file if the file is missing.
-  path    => '/tmp/school'
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
