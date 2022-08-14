e91a9fce7c1927fffd66810fb774f38bb92a859f
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-08-13 19:41:50 -0700

    TASK

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 #Connect with puppet without password
-https://doc.wikimedia.org/puppet/puppet_types/file_line.html
+#https://doc.wikimedia.org/puppet/puppet_types/file_line.html
 file_line { 'Title':
  ensure => 'present',
  path => '/etc/ssh/ssh_config', #File to append the line
- line => '	IdentityFile ~/.ssh/school' #Line to append
+ line => '      IdentityFile ~/.ssh/school' #Line to append
 }
 
 file_line { 'No_Passwd':
   path => '/etc/ssh/ssh_config', #File to append the line
-  line => '	PasswordAuthentication no' #Line to append
+  line => '     PasswordAuthentication no' #Line to append
 }
+
