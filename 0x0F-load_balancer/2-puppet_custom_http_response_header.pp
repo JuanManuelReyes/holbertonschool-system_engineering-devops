626464d2e7950a04d1be993405355b5035352729
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-08-22 11:58:41 -0700

    Fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,23 @@
 #Using Puppet
-exec { 'apt-update':
-  command => 'sudo apt-get -y update',
+exec { 'apt-get update':
+  command  => 'sudo apt-get -y update',
   provider => shell,
+  before   => Exec['InstallNginx'],
 }
 
-exec { 'InstallNginx':
-  command => 'sudo apt -y install nginx',
+exec {'InstallNginx':
+  command  => 'sudo apt-get -y install nginx',
   provider => shell,
+  before   => Exec['append'],
 }
 
 exec { 'append':
- command => "sudo sed -i '/listen 80 default_server/a add_header X-Served-By ${hostname};' /etc/nginx/sites-enabled/default",
- provider => shell,
+  provider => shell,
+  command  => "sudo sed -i '/listen 80 default_server/a add_header X-Served-By ${hostname};' /etc/nginx/sites-enabled/default",
+  before   => Exec['restart'],
 }
 
-exec { 'NginxRestart':
+exec { 'restart':
   command  => 'sudo service nginx restart',
-  provider => shell,
-}
+  provider => 'shell'
+  }
