ade633a5d817d2c429f4371b6176e8fd741745e4
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-06 20:27:29 -0300

    Task 2 fix

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,7 +2,7 @@
 """
 https://jsonplaceholder.typicode.com/
 Using what you did in the task #0, extend your Python script
-to export data in the JSON format.    
+to export data in the JSON format.
 """
 
 import json
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     Display on the standard output the employee TODO list progress.
     """
 
-    USER_ID  = 0
+    USER_ID = 0
 
     # id must be int
     id = int(argv[1])
@@ -26,7 +26,7 @@ if __name__ == '__main__':
             USER_ID = user.get("id")
             USERNAME = user.get("username")
             break
-  
+
     todos = requests.get("https://jsonplaceholder.typicode.com/todos")
 
     todolist = []
