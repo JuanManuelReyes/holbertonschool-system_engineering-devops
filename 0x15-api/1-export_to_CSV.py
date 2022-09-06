81514e6a6aaf90449a7feed7bb328b3d7e206b4e
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-06 19:59:52 -0300

    Task 0 fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,7 +2,7 @@
 """
 https://jsonplaceholder.typicode.com/
 Using what you did in the task #0, extend your Python
-script to export data in the CSV format.    
+script to export data in the CSV format.
 """
 
 import csv
@@ -14,12 +14,12 @@ if __name__ == '__main__':
     Display on the standard output the employee TODO list progress.
     """
 
-    USER_ID  = 0
+    USER_ID = 0
     USERNAME = ""
     TAS_COMPLETED_STATUS = False
     TASK_TITLE = ""
 
-    #id must be int
+    """id must be int"""
     id = int(argv[1])
 
     users = requests.get("https://jsonplaceholder.typicode.com/users")
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     todos = requests.get("https://jsonplaceholder.typicode.com/todos")
 
     file_name = "{}.csv".format(USER_ID)
-    with open(file_name, "w", encoding = 'utf-8') as file:
+    with open(file_name, "w", encoding="utf-8") as file:
         for task in todos.json():
             if task.get("userId") == id:
                 TASK_TITLE = task.get("title")
@@ -45,4 +45,3 @@ if __name__ == '__main__':
                 file.write('"{}","{}","{}","{}"\n'
                            .format(USER_ID, USERNAME, TASK_COMPLETED_STATUS,
                                    TASK_TITLE))
-
