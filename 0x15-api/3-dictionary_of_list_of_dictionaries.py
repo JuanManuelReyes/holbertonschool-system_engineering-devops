893f328b60032e59a2463e1cd9f530c9aef7b1dc
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-06 20:42:33 -0300

    Task 3 fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,10 +1,39 @@
 #!/usr/bin/python3
 """
 https://jsonplaceholder.typicode.com/
-Using what you did in the task #0, extend your Python script to export data in the JSON format.   
+Using what you did in the task #0, extend your Python script
+to export data in the JSON format.
 """
+
+import json
+import requests
+from sys import argv
 
 if __name__ == '__main__':
     """
-    
+    Display on the standard output the employee TODO list progress.
     """
+
+    USER_ID = 0
+
+    # id must be int
+    id = int(argv[1])
+
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    
+    todolist = []
+    tododict = {}
+    for user in users.json():
+        USER_ID = user.get("id")
+        USERNAME = user.get("username")
+        with open("todo_all_employees.json", "w", encoding="utf-8") as file:
+            for task in todos.json():
+                dict = {}
+                if user.get("id") == id:
+                    dict['task'] = task['title']
+                    dict['completed'] = task['completed']
+                    dict['username'] = USERNAME
+                    todolist.append(dict)
+            tododict[id] = todolist
+            file.write(json.dumps(tododict))
