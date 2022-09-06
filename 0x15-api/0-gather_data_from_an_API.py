4650504788bf9c5a38d05c0d4ed4807a5ccb4ae0
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-06 19:54:59 -0300

    Task 0 fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,24 +11,24 @@ if __name__ == '__main__':
     """
     Display on the standard output the employee TODO list progress.
     """
-    
+
     EMPLOYEE_NAME = ""
     TOTAL_NUMBER_OF_TASKS = 0
     NUMBER_OF_DONE_TASKS = 0
     TASK_TITLE = []
-    
-    #id must be int
+
+    """id must be int"""
     id = int(argv[1])
-    
+
     users = requests.get("https://jsonplaceholder.typicode.com/users")
-    
+
     for user in users.json():
         if user.get('id') == id:
             EMPLOYEE_NAME = user.get('name')
             break
-            
+
     todos = requests.get("https://jsonplaceholder.typicode.com/todos")
-    
+
     for task in todos.json():
         if task.get("userId") == id:
             TOTAL_NUMBER_OF_TASKS += 1
@@ -36,6 +36,9 @@ if __name__ == '__main__':
                 NUMBER_OF_DONE_TASKS += 1
                 TASK_TITLE.append(task.get("title"))
 
-    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUMBER_OF_TASKS
+                                                          ))
     for task in TASK_TITLE:
         print("\t {}".format(task))
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,7 +4,35 @@ https://jsonplaceholder.typicode.com/
 Using what you did in the task #0, extend your Python script to export data in the JSON format.    
 """
 
+import json
+import requests
+from sys import argv
+
 if __name__ == '__main__':
     """
-    
+    Display on the standard output the employee TODO list progress.
     """
+
+    USER_ID  = 0
+    TASK_TITLE = []
+    
+    #id must be int
+    id = int(argv[1])
+    
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    
+    for user in users.json():
+        if user.get("id") == id:
+            USER_ID = user.get("id")
+            USERNAME = user.get("username")
+            break
+            
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    
+    dict = {}
+        
+        
+    
+    file_name = "{}.json".format(USER_ID)
+    with open(file_name, "w", encoding = 'utf-8') as file:
+        json.dump()
