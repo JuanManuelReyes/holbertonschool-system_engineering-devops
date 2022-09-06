aa569cf66bb541f65019fe0e7e4885a6a7b3b68f
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-06 00:51:04 -0300

    Files

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+https://jsonplaceholder.typicode.com/
+For a given employee ID, returns information about his/her TODO list progress.
+"""
+
+if __name__ == '__main__':
+    """
+    
+    """
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+https://jsonplaceholder.typicode.com/
+Using what you did in the task #0, extend your Python script to export data in the JSON format.   
+"""
+
+if __name__ == '__main__':
+    """
+    
+    """
