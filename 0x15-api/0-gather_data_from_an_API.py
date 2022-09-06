2b36fa0a44c5df9c3fc4c4e7843e5761aeba778c
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-09-06 05:45:12 -0700

    Task 0 Done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     EMPLOYEE_NAME = ""
     TOTAL_NUMBER_OF_TASKS = 0
     NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = 0
+    TASK_TITLE = []
     
     #id must be int
     id = int(argv[1])
@@ -34,5 +34,8 @@ if __name__ == '__main__':
             TOTAL_NUMBER_OF_TASKS += 1
             if task.get("completed") is True:
                 NUMBER_OF_DONE_TASKS += 1
-                
-    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, NUMBER_OF_DONE_TASKS))
+                TASK_TITLE.append(task.get("title"))
+
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for task in TASK_TITLE:
+        print("\t{}".format(task))
