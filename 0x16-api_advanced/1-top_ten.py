797d24cd35b53390e39be0d416cb146523921f62
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-21 16:17:40 -0300

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,24 +3,25 @@
 
 import requests
 
+
 def top_ten(subreddit):
-    """ 
+    """
     Return the sub number
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    
+
     headers = requests.utils.default_headers()
-    
+
     headers.update(
     {
         'User-Agent': 'My User Agent 1.0',
     }
     )
-    
+
     res = requests.get(url, headers=headers)
-    
+
     res_json = res.json()
-    
+
     if res.status_code <= 200:
         counter = 0
         while (counter < 10):
