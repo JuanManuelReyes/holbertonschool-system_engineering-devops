c6e8ef62c05643002449d277fd1fdaa22fb714d1
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-21 16:32:21 -0300

    ?

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""asd"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    Return the sub number
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    headers = requests.utils.default_headers()
+
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    res = requests.get(url, headers=headers)
+
+    res_json = res.json()
