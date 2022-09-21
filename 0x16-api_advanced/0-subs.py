b728a0e7417d79d24dfcfca8f57f68b2f8a7f059
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-21 16:18:28 -0300

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,8 +3,9 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """ 
+    """
     Return the sub number
     https://stackoverflow.com/questions/
     10606133/%20sending-user-agent-using-requests-library-in-python
@@ -13,19 +14,19 @@ def number_of_subscribers(subreddit):
     Status#successful_responses
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    
+
     headers = requests.utils.default_headers()
-    
+
     headers.update(
     {
         'User-Agent': 'My User Agent 1.0',
     }
     )
-    
+
     res = requests.get(url, headers=headers)
-    
+
     res_json = res.json()
-    
+
     if res.status_code <= 200:
         return res_json['data']['subscribers']
     else:
