fb0abdd76b0834e54042a28253afee6d45704001
Juan Reyes <73147324+JuanManuelReyes@users.noreply.github.com>
2022-09-22 08:27:07 -0300

    requirement?

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# https://stackoverflow.com/questions/4731364/internal-error-500-apache-but-nothing-in-the-logs
+
 exec { 'fix_error':
   command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
