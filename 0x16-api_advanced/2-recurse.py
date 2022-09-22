415a2a4a5ca9b995ea47c9bdc3c21058f48755b0
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-21 22:24:54 -0300

    Final Task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,16 +4,33 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """
     Return the sub number
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    headers = requests.utils.default_headers()
+        headers = requests.utils.default_headers()
 
-    headers.update({'User-Agent': 'My User Agent 1.0'})
+        headers.update({'User-Agent': 'My User Agent 1.0'})
 
-    res = requests.get(url, headers=headers)
+        res = requests.get(url, headers=headers, params={'after': after})
 
-    res_json = res.json()
+        res_json = res.json()
+        
+        child = res_json['data']['children']
+        
+        for title in child:
+            hot_list.append(title['data']['title'])
+            
+        after = res_json['data']['after']
+        
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        
+        return hot_list
+    
+    except Exception:
+        return None
+    
