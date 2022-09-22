3442696970fdf1d24739102489a33149c47efe16
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-21 22:26:11 -0300

    Fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,19 +18,18 @@ def recurse(subreddit, hot_list=[], after=None):
         res = requests.get(url, headers=headers, params={'after': after})
 
         res_json = res.json()
-        
+
         child = res_json['data']['children']
-        
+
         for title in child:
             hot_list.append(title['data']['title'])
-            
+
         after = res_json['data']['after']
-        
+
         if after is not None:
             recurse(subreddit, hot_list, after)
-        
+
         return hot_list
-    
+
     except Exception:
         return None
-    
