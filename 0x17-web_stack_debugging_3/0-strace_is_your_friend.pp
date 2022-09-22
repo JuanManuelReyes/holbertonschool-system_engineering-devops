804fe6d5d286a4782a1c91657b8cfcb62e3668e6
Juan Reyes <73147324+JuanManuelReyes@users.noreply.github.com>
2022-09-22 08:30:29 -0300

    change

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # https://stackoverflow.com/questions/4731364/internal-error-500-apache-but-nothing-in-the-logs
 
 exec { 'fix_error':
-  command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-  path => "/usr/bin/env",
+  command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
 }
