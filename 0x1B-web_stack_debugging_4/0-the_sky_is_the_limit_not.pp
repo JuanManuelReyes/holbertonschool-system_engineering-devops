4f64ac5e280980a41851d61fa375458db6f431ce
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-26 19:44:11 -0300

    Some Tasks

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+exec { 'fix_error':
+  command  => "/usr/bin/env sudo sed -i 's/15/2000/g' /etc/default/nginx"
+}
+exec { 'restart':
+  command  => 'sudo service nginx restart',
+  provider => 'shell'
+}
