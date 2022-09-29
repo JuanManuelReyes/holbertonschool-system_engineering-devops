29002b1be3851c2f962eeaba59c89f1d557f8071
Juan Reyes <73147324+JuanManuelReyes@users.noreply.github.com>
2022-09-29 15:32:11 -0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 exec { 'fix_error':
-  command  => "/usr/bin/env sudo sed -i 's/ULIMIT=\'-n 15\'/ULIMIT=\'-n 2000\'/' /etc/default/nginx"
+  command  => "/usr/bin/env sudo sed -i 's/ULIMIT=\'-n 15\'/ULIMIT=\'-n 2000\'/' /etc/default/nginx",
+  provider => 'shell'
 }
 exec { 'restart':
   command  => 'sudo service nginx restart',
