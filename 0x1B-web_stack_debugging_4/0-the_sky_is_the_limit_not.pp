aa6eab82677868cda1144722f90d558264cf000e
Juan Reyes <73147324+JuanManuelReyes@users.noreply.github.com>
2022-09-29 15:42:47 -0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# Comment
+# Change limit of requests for nginx
 
-# Comment
-exec { 'fix_error':
+# Se cambia el limite de requests para nginx
+exec {'replace':
+  provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2000\"/" /etc/default/nginx',
-  provider => shell
 }
 
-# Comment
-exec { 'restart':
-  command  => 'sudo service nginx restart',
-  provider => shell
+# Restart Nginx para que queden los cambios
+exec { 'restart_nginx': # se puede poner cualquier nombre aca
+  command  => 'sudo service nginx restart', # Aca se le pasa el comando a ejecutar
+  provider => 'shell' # Se le indica donde se va a correr
 }
