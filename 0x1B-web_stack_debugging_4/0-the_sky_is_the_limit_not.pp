1235bfbfafcd4ec96fee6a68fd916f313b9b7e76
Juan Reyes <73147324+JuanManuelReyes@users.noreply.github.com>
2022-09-29 10:25:25 -0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,4 @@
-exec { 'fix_error':
-  command  => "sed -i 's/15/20000/g' /etc/default/nginx",
-  path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-}
-exec { 'restart':
-  command  => 'sudo service nginx restart',
-  path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+exec {'fix_error':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
